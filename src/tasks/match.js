13b99e23df346edc2ed5968c9ce79720dfb1adde
dmytropihur <85562726+dmytropihur@users.noreply.github.com>
2022-06-21 20:58:51 +0200

    Delete src/tasks directory

--- src/tasks/match.js
+++
@@ -1,5 +0,0 @@
-const match = (str) => {
-  const regex = /(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;
-
-  return regex.test(str);
-};
--- src/tasks/sortByLength.js
+++
@@ -1,5 +0,0 @@
-const sortByLength = (arr) => {
-  return arr.sort((a, b) => a.length - b.length);
-};
-
-console.log(sortByLength(['Turing', 'Einstein', 'Jung']));
